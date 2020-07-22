9eb5cdaf55c319c1cae7cc256cc6d7f983ccbc4b
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-22 16:40:13 +0100

    working with video

--- Face_Detector.py
+++ Face_Detector.py
@@ -5,11 +5,33 @@ trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
 
 # choose an img to detect faces in
-img = cv2.imread('groupPhoto.png')
+#img = cv2.imread('groupPhoto.png')
 
-# must convert to greyscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# capture video from webcam
+webcam = cv2.VideoCapture(0)
+key = cv2.waitKey(1)
 
+
+# iterate over frames
+while True:
+    # read current frame
+    succesful_frame_read, frame = webcam.read()
+
+    # must convert to greyscale
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+    # draw rectangles around faces, goes through list staring at 0 and squares each face  it sees
+    for(x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Elvos Face Detector', frame)
+    cv2.waitKey(1)
+
+
+"""
 # detect faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
@@ -22,6 +44,5 @@ for(x, y, w, h) in face_coordinates:
 # show img
 cv2.imshow('Elvos Face Detector', img)
 cv2.waitKey()
-
-
+"""
 print("code complete")
