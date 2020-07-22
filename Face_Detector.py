f333443c2ea6ae7c6f4a3c3484a925a1a0ebd856
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-22 16:15:11 +0100

    made a loop so it recognizes more than 1 face

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,13 +1,26 @@
 import cv2
 
+# load pre trained data on face frontals
 trained_face_data = cv2.CascadeClassifier(
     'haarcascade_frontalface_default.xml')
 
-img = cv2.imread('RDJ.png')
+# choose an img to detect faces in
+img = cv2.imread('groupPhoto.png')
 
+# must convert to greyscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Elvos Face Detector', grayscaled_img)
+# detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# draw rectangles around faces, goes through list staring at 0 and squares each face  it sees
+for(x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# print(face_coordinates)
+
+# show img
+cv2.imshow('Elvos Face Detector', img)
 cv2.waitKey()
 
 
